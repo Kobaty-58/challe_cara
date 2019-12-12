87838389961b0589bb690ca163c293e7e6f3b8fe
conken <conken@Jun-no-MacBook-Air.local>
2019-12-12 09:57:16 +0900

    home画面のリンクを調整

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,25 +2,25 @@
   <v-container fluid>
     <v-row dense>
       <v-col v-for="detail in details" :key="detail.id">
-        <router-link :to="`/detail/${detail.id}`">
-          <v-card>
+        <v-card>
+          <router-link :to="`/detail/${detail.id}`">
             <v-img :src="detail.imgSrc" class="white--text align-end" width="350px" height="300px">
               <v-card-title v-text="detail.title"></v-card-title>
             </v-img>
-            <v-card-actions>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-              <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </router-link>
+          </router-link>
+          <v-card-actions>
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>mdi-bookmark</v-icon>
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>mdi-share-variant</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
