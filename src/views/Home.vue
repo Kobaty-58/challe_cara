2970e7b6746180e8484a0a87de0b1772b7527215
conken <conken@junnoMacBook-ea.local>
2019-12-13 17:54:24 +0900

    Home.vueの修正・改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,26 @@
 <template>
   <v-container fluid>
     <v-row dense>
-
         <v-col v-for="photo in photos" :key="photo.id" :photo="photo">
         <router-link :to="`/photo/${photo.id}`">
           <v-card>
               <v-img :src="photo.imgSrc" class="white--text align-end" width="350px" height="300px">
               <v-card-title v-text="photo.title"></v-card-title>
-
             </v-img>
-          </router-link>
-          <v-card-actions>
-            <v-btn icon>
-              <v-icon>mdi-heart</v-icon>
-            </v-btn>
-            <v-btn icon>
-              <v-icon>mdi-bookmark</v-icon>
-            </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>mdi-share-variant</v-icon>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
+            <v-card-actions>
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </router-link>
       </v-col>
     </v-row>
   </v-container>
